f83d93ed0504dfab5ebfeab923eb027a56936a82
dankato <apearmor@gmail.com>
2017-09-07 12:26:02 -0700

    exercise 1 & 2 completed

---
+++ searching-exercises.js
@@ -0,0 +1,28 @@
+console.log("epic sauce possum awesome");
+
+// Exercise 1
+const dataset = "89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5 76 62";
+
+
+let array = dataset.split(' ');
+const data = array.map(number => parseInt(number));
+const sortData = data.sort(sortNumber);
+console.log(sortData);
+
+function findValue(dataset, value) {
+	for(let i = 0; i < dataset.length; i++) {
+		if(dataset[i] == value) {
+			return i;
+		}
+	}
+	return -1; // found nothing
+}; 
+
+console.log(findValue(dataset, 30)); // 1
+
+// Exercise 2
+
+function sortNumber(a,b) {
+    return a - b;
+}
+
